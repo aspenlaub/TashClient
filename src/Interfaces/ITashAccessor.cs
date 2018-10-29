b43201745e1ad647a005f74632c25c2f80680420
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-10-29 18:21:04 +0100

    Dvin update

--- src/Interfaces/ITashAccessor.cs
+++ src/Interfaces/ITashAccessor.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Net;
 using System.Threading.Tasks;
 using Aspenlaub.Net.GitHub.CSharp.Dvin.Entities;
-using Aspenlaub.Net.GitHub.CSharp.PeghStandard.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Tash.Model;
 
 namespace Aspenlaub.Net.GitHub.CSharp.TashClient.Interfaces {
